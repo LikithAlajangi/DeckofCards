fc6ca7ec63a850295b6eaaa3a5e6a6e6ce92c9cf
U-IN\Likith.Alajangi <Likith.Alajangi@LT0204953.in.ril.com>
2024-11-04 16:19:59 +0530

    implemented functions in main and excecuted in main class

---
+++ Main.java
@@ -0,0 +1,14 @@
+package Cards;
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  List<Card> deck = Card.getDeck();	
+		  Card.printDeck(deck);
+		  
+		}
+
+	}
+
+
